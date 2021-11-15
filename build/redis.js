63923699e0f4a9fe1a8064f51b2db9e6b2457787
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2021-11-15 20:47:16 +0000

    chore: debug

--- build/redis.js
+++ build/redis.js
@@ -6,12 +6,12 @@ var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connection = void 0;
 const ioredis_1 = __importDefault(require("ioredis"));
-const localRedisUrl = "redis://127.0.0.1";
+const localRedisUrl = "redis://127.0.0.1:6379";
 const redisUrl = ((_a = process.env.REDIS_URL) === null || _a === void 0 ? void 0 : _a.slice(0, process.env.REDIS_URL.lastIndexOf(":"))) ||
     localRedisUrl;
-const isLocal = redisUrl === localRedisUrl;
+console.log(redisUrl);
 const splitByColon = redisUrl.split(":");
-const port = isLocal ? 6379 : Number(splitByColon[splitByColon.length - 1]);
+const port = Number(splitByColon[splitByColon.length - 1]);
 exports.connection = new ioredis_1.default({
     host: redisUrl,
     port,
--- src/redis.ts
+++ src/redis.ts
@@ -1,12 +1,12 @@
 import IORedis from "ioredis";
 
-const localRedisUrl = "redis://127.0.0.1";
+const localRedisUrl = "redis://127.0.0.1:6379";
 const redisUrl =
   process.env.REDIS_URL?.slice(0, process.env.REDIS_URL.lastIndexOf(":")) ||
   localRedisUrl;
-const isLocal = redisUrl === localRedisUrl;
+console.log(redisUrl);
 const splitByColon = redisUrl.split(":");
-const port = isLocal ? 6379 : Number(splitByColon[splitByColon.length - 1]);
+const port = Number(splitByColon[splitByColon.length - 1]);
 
 export const connection = new IORedis({
   host: redisUrl,
